b3ef153b9493b470b766b8ed19ea38c88b50f8ce
Chris Perry <cmperry@live.co.uk>
2024-02-04 21:02:36 +0000

    -hot fix for static files

--- src/app/api/images/route.tsx
+++ src/app/api/images/route.tsx
@@ -4,7 +4,7 @@ import fs from "fs";
 export async function GET(request: Request) {
     var preImages: string[] = ["None"];
     var images = fs.readdirSync(
-        (process.cwd() + process.env.PUT_STATIC_IMAGES) as string
+        (process.cwd() + process.env.STATIC_IMAGES) as string
     );
     var result = preImages.concat(images);
     return new NextResponse(JSON.stringify(result), { status: 201 });
--- src/app/api/videos/route.tsx
+++ src/app/api/videos/route.tsx
@@ -4,7 +4,7 @@ import fs from "fs";
 export async function GET(request: Request) {
     var bgVideos: string[] = ["None"];
     var videos = fs.readdirSync(
-        (process.cwd() + process.env.PUT_STATIC_VIDEOS) as string
+        (process.cwd() + process.env.STATIC_VIDEOS) as string
     );
     var temp = bgVideos.concat(videos);
     return new NextResponse(JSON.stringify(temp), { status: 201 });
