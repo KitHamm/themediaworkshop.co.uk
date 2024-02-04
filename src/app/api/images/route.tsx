5572c0c633f63d11b0b9310201d9eee52aba5cc0
Chris Perry <cmperry@live.co.uk>
2024-02-04 21:37:19 +0000

    -trying images again

--- src/app/api/images/route.tsx
+++ src/app/api/images/route.tsx
@@ -4,8 +4,10 @@ import fsp from "fs/promises";
 
 export async function GET(request: Request) {
     var preImages: string[] = ["None"];
+    // var images = await fsp.readdir(
+    //     (process.cwd() + process.env.STATIC_IMAGES) as string
     var images = await fsp.readdir(
-        (process.cwd() + process.env.STATIC_IMAGES) as string
+        process.env.NEXT_PUBLIC_BASE_IMAGE_URL as string
     );
     var result = preImages.concat(images);
     return new NextResponse(JSON.stringify(result), { status: 201 });
