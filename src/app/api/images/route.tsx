5f39c067247f46af5c723ee3184a8335d145f1e1
Chris Perry <cmperry@live.co.uk>
2024-02-04 21:00:30 +0000

    -added order to segment

--- src/app/api/images/route.tsx
+++ src/app/api/images/route.tsx
@@ -3,7 +3,9 @@ import fs from "fs";
 
 export async function GET(request: Request) {
     var preImages: string[] = ["None"];
-    var images = fs.readdirSync(process.env.STATIC_IMAGES as string);
+    var images = fs.readdirSync(
+        (process.cwd() + process.env.PUT_STATIC_IMAGES) as string
+    );
     var result = preImages.concat(images);
     return new NextResponse(JSON.stringify(result), { status: 201 });
 }
