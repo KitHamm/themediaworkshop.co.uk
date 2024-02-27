f07ee935c625b4eba61bafa2de791a2b29167903
Chris Perry <cmperry@live.co.uk>
2024-02-27 09:35:18 +0000

    -possible fix for download images being corrupt

--- src/components/dashboard/Media.tsx
+++ src/components/dashboard/Media.tsx
@@ -582,8 +582,11 @@ export default function Media(props: {
                                 )}
                                 <a
                                     className="transition-all hover:bg-opacity-85 text-sm bg-orange-600 flex items-center px-2 py-1 rounded-xl"
-                                    href="/"
-                                    download={selectedImage}>
+                                    href={
+                                        process.env.NEXT_PUBLIC_BASE_IMAGE_URL +
+                                        selectedImage
+                                    }
+                                    download>
                                     Download
                                 </a>
                                 <Button
