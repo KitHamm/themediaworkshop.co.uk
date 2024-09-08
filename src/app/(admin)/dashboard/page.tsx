d4eb9320ecf91dda487769b1d6109cc8523d6f21
Kit Hamm <kitmakesthings@gmail.com>
2024-09-08 11:12:17 +0100

    - dashboard now uses layout for side panel

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,25 +1,16 @@
 //  Prisma
 import prisma from "@/lib/prisma";
 //  Components
-import SidePanel from "@/components/dashboard/SidePanel";
 import DashboardMain from "@/components/dashboard/DashboardMain";
 //  Types
-import { Message, Page, Segment, CaseStudy, emailHost } from "@prisma/client";
+import { Message, emailHost } from "@prisma/client";
 
-interface ExtendedPage extends Page {
-    segment: ExtendedSegment[];
-}
-interface ExtendedSegment extends Segment {
-    casestudy: CaseStudy[];
-}
 //  Functions
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/authOptions";
 import { revalidatePath } from "next/cache";
 
 export default async function Dashboard() {
-    var pageData: ExtendedPage[] = null as any;
-    var emailHostData: emailHost = null as any;
     // Function for revalidating the dashboard route as well as the page route that the content relates to
     async function revalidateDashboard(route: string) {
         "use server";
@@ -51,28 +42,17 @@ export default async function Dashboard() {
             { name: "asc" },
         ],
     });
-    if (data !== null) {
-        pageData = data as unknown as ExtendedPage[];
-    }
-    if (emailHost !== null) {
-        emailHostData = emailHost as emailHost;
-    }
+
     return (
-        <main className="xl:flex xl:h-auto min-h-screen">
-            <div className="relative xl:h-auto xl:basis-1/6">
-                {/* Side panel for dashboard showing user information */}
-                <SidePanel messages={messages} session={session} />
-            </div>
-            <div className="xl:basis-5/6 min-h-screen">
-                {/* Main dashboard panel with all views available */}
-                <DashboardMain
-                    emailHost={emailHostData}
-                    messages={messages}
-                    revalidateDashboard={revalidateDashboard}
-                    session={session}
-                    data={pageData}
-                />
-            </div>
-        </main>
+        <>
+            {/* Main dashboard panel with all views available */}
+            <DashboardMain
+                emailHost={emailHost as emailHost}
+                messages={messages}
+                revalidateDashboard={revalidateDashboard}
+                session={session}
+                data={data}
+            />
+        </>
     );
 }
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata, Viewport } from "next";
 import { Lato } from "next/font/google";
 import "../globals.css";
+import SidePanel from "@/components/dashboard/SidePanel";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/authOptions";
+import prisma from "@/lib/prisma";
+import { Message } from "@prisma/client";
 
 const lato = Lato({
     weight: ["100", "300", "400", "700", "900"],
@@ -19,11 +24,20 @@ export const metadata: Metadata = {
         "The Media Workshop is a digital production and development company who work creatively with new media and developing technologies.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const session = await getServerSession(authOptions);
+    const messages: Message[] = await prisma.message.findMany({
+        orderBy: [
+            {
+                createdAt: "desc",
+            },
+            { name: "asc" },
+        ],
+    });
     return (
         <html lang="en">
             <body
@@ -31,7 +45,16 @@ export default function RootLayout({
                     lato.className +
                     " bg-neutral-900 text-white m-0 w-screen overflow-x-hidden"
                 }>
-                {children}
+                <main className="xl:flex xl:h-auto min-h-screen">
+                    <div className="relative xl:h-auto xl:basis-1/6">
+                        {/* Side panel for dashboard showing user information */}
+                        <SidePanel messages={messages} session={session} />
+                    </div>
+                    <div className="xl:basis-5/6 min-h-screen">
+                        {/* Main dashboard panel with all views available */}
+                        {children}
+                    </div>
+                </main>
             </body>
         </html>
     );
