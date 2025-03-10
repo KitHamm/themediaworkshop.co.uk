671ecf9578b632e7be782a394dc69683c6e56030
Kit Hamm <kitmakesthings@gmail.com>
2025-03-10 16:58:22 +0000

    -added public read and early return for null config

--- src/app/api/signed/route.tsx
+++ src/app/api/signed/route.tsx
@@ -1,18 +1,35 @@
 "use server";
 
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+	S3Client,
+	PutObjectCommand,
+	ObjectCannedACL,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NextResponse } from "next/server";
 
 // S3 Configuration
-const bucketName = process.env.SPACES_BUCKET_NAME!;
-const endpoint = process.env.SPACES_ENDPOINT!;
-const region = process.env.SPACES_REGION!;
-const accessKeyId = process.env.SPACES_ACCESS_KEY!;
-const secretAccessKey = process.env.SPACES_SECRET_KEY!;
+const bucketName = process.env.SPACES_BUCKET_NAME;
+const endpoint = process.env.SPACES_ENDPOINT;
+const region = process.env.SPACES_REGION;
+const accessKeyId = process.env.SPACES_ACCESS_KEY;
+const secretAccessKey = process.env.SPACES_SECRET_KEY;
 
 export async function POST(request: Request) {
 	try {
+		if (
+			!region ||
+			!endpoint ||
+			!accessKeyId ||
+			!secretAccessKey ||
+			!bucketName
+		) {
+			return NextResponse.json(
+				{ error: "Spaces configuration incomplete" },
+				{ status: 400 }
+			);
+		}
+
 		const { fileName, folder } = await request.json();
 
 		if (!fileName) {
@@ -33,7 +50,8 @@ export async function POST(request: Request) {
 		const command = new PutObjectCommand({
 			Bucket: bucketName,
 			Key: key,
-			ContentType: "video/mp4", // You can set your content type accordingly
+			ContentType: "video/mp4",
+			ACL: ObjectCannedACL.public_read,
 		});
 
 		const signedUrl = await getSignedUrl(s3Client, command, {
